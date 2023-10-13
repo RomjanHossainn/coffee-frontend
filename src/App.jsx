220ddf1e992696d45d5d74d55e5c7936659220c0
Romjan Hossain <romjanhossainn@gmail.com>
2023-10-13 23:00:12 +0600

    coffee-forntend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Coffe Home page All coffe</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1>Add coffee</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
